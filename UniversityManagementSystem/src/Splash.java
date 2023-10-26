2fad6b510647c3bd2642f96d61bded63e716ba7d
Shakib Hossain <shakibhossain2020124@gmail.com>
2023-10-26 18:39:06 +0600

    Edited Splash

--- UniversityManagementSystem/src/Splash.java
+++ UniversityManagementSystem/src/Splash.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame {
-    ImageIcon splashPic = new ImageIcon("Icons/cover.jpg");
+    ImageIcon i1 = new ImageIcon("Icons/cover.jpg");
+    Image i2 = i1.getImage().getScaledInstance(800,600,Image.SCALE_DEFAULT);
+    ImageIcon splashPic = new ImageIcon(i2);
+
     ImageIcon welcome = new ImageIcon("Icons/welcome.jpeg");
     JPanel mainPanel = new JPanel();
     JLabel mainLabel = new JLabel();
@@ -9,20 +13,22 @@ public class Splash extends JFrame {
     Splash(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
+        this.setSize(800,600);
         this.setTitle("UniversityManagementSystem");
+        this.setLocationRelativeTo(null);
         this.getContentPane().add(mainPanel);
         windowComponents();
-        this.setLocationRelativeTo(null);
         this.setVisible(true);
+
     }
 
     void windowComponents(){
         mainLabel.setIcon(splashPic);
         mainPanel.add(mainLabel);
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(-5,-5,-5,-5));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(-5,0,0,0));
         mainLabel.add(nextPage);
-        this.pack();
-        nextPage.setBounds(mainLabel.getWidth()/2-75, 50,150,50);
+        nextPage.setBackground(new Color(0xA7FECC5, true));
+        nextPage.setBounds(0, 512,800,50);
         nextPage.setIcon(welcome);
         nextPage.addActionListener((e)->{
             System.out.println("Next Page");
